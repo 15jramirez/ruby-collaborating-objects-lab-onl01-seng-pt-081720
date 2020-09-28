fce60cc60d651e848e44c42acec7dab82cf7c5cd
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-28 08:19:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ class Song
     filename_array = filename.split (" - ")
      song = Song.new(filename_array[1]) 
      artist = filename_array[0]
+     binding.pry
      song.artist_name = artist
      song
   end
